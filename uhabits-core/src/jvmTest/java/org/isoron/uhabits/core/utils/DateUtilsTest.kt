09671c746f43d6a7b50a541a276cafbcd2adfb68
eduebernal <eduebernal@gmail.com>
2022-07-06 14:56:15 -0700

    test attempt 3

--- uhabits-core/src/jvmTest/java/org/isoron/uhabits/core/utils/DateUtilsTest.kt
+++ uhabits-core/src/jvmTest/java/org/isoron/uhabits/core/utils/DateUtilsTest.kt
@@ -120,25 +120,25 @@ class DateUtilsTest : BaseUnitTest() {
 
     @Test
     fun getWeekdaysInMonth() {
-        val february = GregorianCalendar(2018, 2, 1)
-        val leapFebruary = GregorianCalendar(2020, 2, 1)
-        val month = GregorianCalendar(2020, 4, 1)
-        val longMonth = GregorianCalendar(2020, 8, 1)
+        val february = GregorianCalendar(2018, Calendar.FEBRUARY, 1)
+        val leapFebruary = GregorianCalendar(2020, Calendar.FEBRUARY, 1)
+        val month = GregorianCalendar(2020, Calendar.APRIL, 1)
+        val longMonth = GregorianCalendar(2020, Calendar.AUGUST, 1)
 
         assertThat(
             arrayOf(4, 4, 4, 4, 4, 4, 4),
             equalTo(DateUtils.getWeekdaysInMonth(Timestamp(february)))
         )
         assertThat(
-            arrayOf(4, 4, 4, 4, 4, 4, 5),
+            arrayOf(5, 4, 4, 4, 4, 4, 4),
             equalTo(DateUtils.getWeekdaysInMonth(Timestamp(leapFebruary)))
         )
         assertThat(
-            arrayOf(4, 4, 4, 5, 5, 4, 4),
+            arrayOf(4, 4, 4, 4, 5, 5, 4),
             equalTo(DateUtils.getWeekdaysInMonth(Timestamp(month)))
         )
         assertThat(
-            arrayOf(5, 5, 4, 4, 4, 4, 5),
+            arrayOf(5, 5, 5, 4, 4, 4, 4),
             equalTo(DateUtils.getWeekdaysInMonth(Timestamp(longMonth)))
         )
     }
